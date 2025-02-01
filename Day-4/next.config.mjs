706d8acda877d90941d69058d2a3224dbf181c7f
Maryam Arif <160311643+maryamarif24@users.noreply.github.com>
2025-02-01 21:17:16 +0500

    Add files via upload

---
+++ Day-4/next.config.mjs
@@ -0,0 +1,8 @@
+// next.config.mjs
+const nextConfig = {
+    images: {
+      domains: ['cdn.sanity.io'], // Add 'cdn.sanity.io' to the list of allowed domains
+    },
+  };
+  
+  export default nextConfig;
